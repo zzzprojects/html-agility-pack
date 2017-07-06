b9c8f5ab5e79e4f45832b2eb60de07e9ae10b690
waqasm78 <waqasm78@gmail.com>
2017-07-06 08:14:26 -0700

    Fixed for new line node

--- src/HtmlAgilityPack/HtmlNode.cs
+++ src/HtmlAgilityPack/HtmlNode.cs
@@ -1918,7 +1918,7 @@ namespace HtmlAgilityPack
                 element = element.NextSibling;
             }
 
-            return count == 1 ? true : false;
+            return count <= 1 ? true : false;
         }
         #endregion
 
