b6fcb3d7f133bb7cf6f549eada0902546e2da19c
zmjack <jack@zmland.com>
2019-11-10 20:56:56 +0800

    201911102056

---
+++ src/HtmlAgilityPack.Shared/XPathVariable.cs
@@ -0,0 +1,30 @@
+﻿/* First released in 'Dawnx', it is now available to HAP under the MIT protocol */
+
+using System.Xml.XPath;
+using System.Xml.Xsl;
+
+namespace HtmlAgilityPack
+{
+    internal class XPathVariable : IXsltContextVariable
+    {
+        public string Prefix { get; private set; }
+        public string Name { get; private set; }
+
+        public XPathVariable(string prefix, string name)
+        {
+            Prefix = prefix;
+            Name = name;
+        }
+
+        public object Evaluate(XsltContext xsltContext)
+        {
+            var argList = ((XPathContext)xsltContext).ArgList;
+            return argList.GetParam(Name, xsltContext.LookupNamespace(Prefix));
+        }
+
+        public bool IsLocal => false;
+        public bool IsParam => true;
+        public XPathResultType VariableType => XPathResultType.Any;
+
+    }
+}
