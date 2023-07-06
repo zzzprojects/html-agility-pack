24471d2d55276fcecd905d70822ec749f5cd1f28
Nedim Basic <basicn86@gmail.com>
2023-07-06 12:03:24 -0400

    Added ability to set the maximum number of redirects

--- src/HtmlAgilityPack.Shared/HtmlWeb.cs
+++ src/HtmlAgilityPack.Shared/HtmlWeb.cs
@@ -89,6 +89,7 @@ namespace HtmlAgilityPack
 
         private string _cachePath;
         private bool _fromCache;
+        private int _maxAutoRedirects = 50;
         private int _requestDuration;
         private Uri _responseUri;
         private HttpStatusCode _statusCode = HttpStatusCode.OK;
@@ -801,6 +802,15 @@ namespace HtmlAgilityPack
         /// <summary>Gets or sets the automatic decompression.</summary>
         /// <value>The automatic decompression.</value>
         public DecompressionMethods AutomaticDecompression { get; set; }
+
+        /// <summary>
+        /// Maximum number of redirects that will be followed. Default value is 50.
+        /// </summary>
+        public int MaxAutoRedirects
+        {
+            set { if (value <= 0) { throw new ArgumentOutOfRangeException(); } else { _maxAutoRedirects = value; } }
+            get { return _maxAutoRedirects; }
+        }
 
         /// <summary>
         /// Gets or sets the timeout value in milliseconds. Must be greater than zero. A value of -1 sets the timeout to be infinite.
@@ -1581,6 +1591,7 @@ namespace HtmlAgilityPack
             bool oldFile = false;
 
             req = WebRequest.Create(uri) as HttpWebRequest;
+            req.MaximumAutomaticRedirections = MaxAutoRedirects;
             req.Timeout = Timeout;
             req.Method = method;
             req.UserAgent = UserAgent;
@@ -1853,6 +1864,7 @@ namespace HtmlAgilityPack
             using (var client = new HttpClient(handler))
             {
                 client.Timeout = TimeSpan.FromMilliseconds(Timeout);
+                handler.MaxAutomaticRedirections = MaxAutoRedirects;
 
                 if(CaptureRedirect)
                 {
