9350bc42b13a2d960a3b69fde00ed5a831c06889
Jonathan Magnan <info@zzzprojects.com>
2023-12-22 15:01:48 -0500

    Update assembly version

--- src/HtmlAgilityPack.UAP10/Properties/AssemblyInfo.cs
+++ src/HtmlAgilityPack.UAP10/Properties/AssemblyInfo.cs
@@ -23,7 +23,7 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.11.56")]
-[assembly: AssemblyFileVersion("1.11.56")]
+[assembly: AssemblyVersion("1.11.57")]
+[assembly: AssemblyFileVersion("1.11.57")]
 
 [assembly: ComVisible(false)]
