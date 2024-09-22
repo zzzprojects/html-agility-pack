670a0f8738656efb0d1a25f67377c39f520f50ab
Jonathan Magnan <info@zzzprojects.com>
2024-09-22 08:52:59 -0400

    Update assembly version

--- src/HtmlAgilityPack.Net45/Properties/AssemblyInfo.cs
+++ src/HtmlAgilityPack.Net45/Properties/AssemblyInfo.cs
@@ -24,10 +24,10 @@ using System.Security;
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(true)]
 [assembly: Guid("643622ea-d2aa-4572-a2b2-6202b7fcd83f")]
-[assembly: AssemblyVersion("1.11.65")]
+[assembly: AssemblyVersion("1.11.66")]
 #if !PocketPC
-[assembly: AssemblyFileVersion("1.11.65")]
-[assembly: AssemblyInformationalVersion("1.11.65")]
+[assembly: AssemblyFileVersion("1.11.66")]
+[assembly: AssemblyInformationalVersion("1.11.66")]
 #if !SILVERLIGHT
 [assembly: AllowPartiallyTrustedCallers]
 #endif
