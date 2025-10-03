64a718a815a6a25e03a73d6c53dfc58a7ed692d3
Jonathan Magnan <info@zzzprojects.com>
2025-10-03 10:37:05 -0400

    Update assembly version

--- src/HtmlAgilityPack.Portable-wp81/Properties/AssemblyInfo.cs
+++ src/HtmlAgilityPack.Portable-wp81/Properties/AssemblyInfo.cs
@@ -24,10 +24,10 @@ using System.Security;
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(true)]
 [assembly: Guid("643622ea-d2aa-4572-a2b2-6202b7fcd83f")]
-[assembly: AssemblyVersion("1.12.3")]
+[assembly: AssemblyVersion("1.12.4")]
 #if !PocketPC
-[assembly: AssemblyFileVersion("1.12.3")]
-[assembly: AssemblyInformationalVersion("1.12.3")]
+[assembly: AssemblyFileVersion("1.12.4")]
+[assembly: AssemblyInformationalVersion("1.12.4")]
 #if !SILVERLIGHT
 [assembly: AllowPartiallyTrustedCallers]
 #endif
